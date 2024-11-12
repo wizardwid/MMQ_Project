db360614731277c88574fc80a67a6c489918c333
wizardwid <s2335@e-mirim.hs.kr>
2024-11-12 23:05:44 +0900

    다시 복구

--- category.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, session
-
-category_bp = Blueprint('category', __name__, url_prefix='/category')
-
-# 카테고리 페이지 라우트 (로그인 후 접근 가능)
-@category_bp.route('/')
-def category():
-    return render_template('category.html')  # 카테고리 페이지 렌더링
